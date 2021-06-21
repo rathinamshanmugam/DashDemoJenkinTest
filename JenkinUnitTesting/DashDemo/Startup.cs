06316c8adc25a82f4647bea87d256fc16d69d33c
Rathinam <rathinam@zilliontechnologies.com>
2021-06-21 19:22:34 +0530

    test new for error

--- JenkinUnitTesting/DashDemo/Startup.cs
+++ JenkinUnitTesting/DashDemo/Startup.cs
@@ -28,7 +28,7 @@ namespace DashDemo
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
+            //test
            
             if (env.IsDevelopment())
             {
